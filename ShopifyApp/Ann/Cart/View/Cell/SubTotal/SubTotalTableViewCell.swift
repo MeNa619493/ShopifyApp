40b113e4894c5124a8476746b96cfa8a4b851975
ann <annmohammed96@gmail.com>
2022-07-05 00:02:30 +0200

    finishing designing

---
+++ ShopifyApp/Ann/Cart/View/Cell/SubTotal/SubTotalTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  SubTotalTableViewCell.swift
+//  ShopifyApp
+//
+//  Created by Ann mohammed on 04/07/2022.
+//  Copyright © 2022 Mina Ezzat. All rights reserved.
+//
+
+import UIKit
+
+class SubTotalTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
--- ShopifyApp/NetworkManager/UrlServices.swift
+++ ShopifyApp/NetworkManager/UrlServices.swift
@@ -11,7 +11,7 @@ import Foundation
 struct UrlServices {
     var endPoint: String = ""
     var url: String  {
-       return "https://7d67dd63dc90e18fce08d1f7746e9f41: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@ios-q3-mansoura.myshopify.com/admin/api/2022-01/\(endPoint)"
+        return "https://7d67dd63dc90e18fce08d1f7746e9f41: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@ios-q3-mansoura.myshopify.com/admin/api/2022-01/\(endPoint)"
     }
 }
 
